595df2bf9b0b87844fdccaa020bf7f44a347ad2e
jameswang <james.wang.dev712@gmail.com>
2022-12-19 14:49:01 -0500

    changed name

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -78,13 +78,31 @@ const Brand = () => {
   return (
     <>
       <div className="flex flex-shrink-0 items-center px-4">
-        <img
+        {/* <img
           className="h-7 w-auto"
           src="https://static.thenounproject.com/png/2018162-200.png"
-          alt="Landsldie"
+          alt="Raise More"
+        /> */}
+        {/* <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          className="w-7 h-7"
+        >
+          <path
+            fillRule="evenodd"
+            d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z"
+            clipRule="evenodd"
+          />
+        </svg> */}
+        <img
+          className="h-7 w-auto"
+          style={{ marginTop: "2px" }}
+          src="https://cdn-icons-png.flaticon.com/512/5842/5842958.png"
+          alt="Raise More"
         />
 
-        <span className="text-xl font-bold mx-3 text-gray-900">Landslide</span>
+        <span className="text-xl font-bold mx-3 text-gray-900">Raise More</span>
       </div>
     </>
   );
