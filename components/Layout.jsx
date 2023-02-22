1076905028d62d5944b2a2e0b8da042e0d6e78c2
jameswang <james.wang.dev712@gmail.com>
2023-02-22 15:54:10 -0500

    styling blue, white bg

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -307,7 +307,7 @@ const Layout = ({ children }) => {
                     </div>
                 </div>
                 <div className="flex flex-1 flex-col md:pl-64">
-                    <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white shadow-sm">
+                    <div className="sticky top-0 z-10 flex h-16 flex-shrink-0 bg-white border-b">
                         <button
                             type="button"
                             className="border-r border-gray-200 px-4 text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500 md:hidden"
