2959a30953da1b0bba3019adb99ae083ece91c87
jameswang <james.wang.dev712@gmail.com>
2023-02-23 10:48:04 -0500

    fix button margin

--- components/QueryBuilderProvider.jsx
+++ components/QueryBuilderProvider.jsx
@@ -109,7 +109,7 @@ export default function QueryBuilderProvider({ table, children, listID }) {
 
                     <button
                         onClick={addRule}
-                        className="my-1 ml-3 relative inline-block justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
+                        className="my-1 mx-3 relative inline-block justify-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100"
                     >
                         Add Filter Step
                     </button>
