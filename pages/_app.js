caf71778d1f7132a089f5e2df9098cca8fda652e
jameswang <james.wang.dev712@gmail.com>
2023-02-24 10:07:53 -0500

    trasnition intercom to papercups

--- pages/_app.js
+++ pages/_app.js
@@ -8,16 +8,45 @@ import "../styles/globals.css";
 
 import Layout from "../components/Layout";
 import { ClerkProvider } from "@clerk/nextjs";
-import { IntercomProvider } from "../utils/IntercomProvider";
+// import { IntercomProvider } from "../utils/IntercomProvider";
+import { ChatWidget } from "@papercups-io/chat-widget";
 
 function App({ Component, pageProps }) {
     return (
         <ClerkProvider {...pageProps}>
-            <IntercomProvider>
-                <Layout>
-                    <Component {...pageProps} />
-                </Layout>
-            </IntercomProvider>
+            {/* <IntercomProvider> */}
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+            <ChatWidget
+                // `accountId` is used instead of `token` in older versions
+                // of the @papercups-io/chat-widget package (before v1.2.x).
+                // You can delete this line if you are on the latest version.
+                // accountId="19650de6-84fa-4da9-a8b9-78ffcadab983"
+                token="19650de6-84fa-4da9-a8b9-78ffcadab983"
+                inbox="1a5d07de-76b2-4430-a1c3-68507bb8b977"
+                title="Welcome to RaiseMore"
+                subtitle="Ask us anything in the chat window below 😊"
+                primaryColor="#2d28ff"
+                greeting="Hello! Any questions?"
+                newMessagePlaceholder="Start typing..."
+                showAgentAvailability={false}
+                agentAvailableText="We're online right now!"
+                agentUnavailableText="We're away at the moment."
+                requireEmailUpfront={false}
+                iconVariant="outlined"
+                baseUrl="https://app.papercups.io"
+                // Optionally include data about your customer here to identify them
+                // customer={{
+                //   name: __CUSTOMER__.name,
+                //   email: __CUSTOMER__.email,
+                //   external_id: __CUSTOMER__.id,
+                //   metadata: {
+                //     plan: "premium"
+                //   }
+                // }}
+            />
+            {/* </IntercomProvider> */}
         </ClerkProvider>
     );
 }
