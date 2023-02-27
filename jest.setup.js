46028ed8d4ee05d729b6f2e9de9f798bd6f94785
jameswang <james.wang.dev712@gmail.com>
2023-02-27 15:17:30 -0500

    allow test in prod

--- jest.setup.js
+++ jest.setup.js
@@ -4,16 +4,3 @@
 // Used for __tests__/testing-library.js
 // Learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom/extend-expect";
-
-// hide error messages about act() being unsupported in production build
-const ignoredErrors = [
-    /act\(\.\.\.\) is not supported in production builds of React/,
-];
-
-const consoleError = global.console.error;
-global.console.error = (...args) => {
-    if (ignoredErrors.some((el) => el.test(args[0]))) {
-        return console.log(...args);
-    }
-    return consoleError(...args);
-};
