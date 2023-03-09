b75af3e55a75ef9a8082180e8289e2c18b01f4ac
jameswang <james.wang.dev712@gmail.com>
2023-03-09 18:27:11 -0500

    fix algolia placeholder errors, render on signin only

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -304,47 +304,26 @@ const Layout = ({ children }) => {
                             <Bars3BottomLeftIcon className="h-6 w-6" aria-hidden="true" />
                         </button>
                         <div className="flex flex-1 justify-between px-4">
-                            <div className="flex flex-1">
-                                <form className="flex w-full md:ml-0" action="#" method="GET">
-                                    {/* <label htmlFor="search-field" className="sr-only">
-                    Search
-                  </label> */}
-                                    {/* <InstantSearch
-                    indexName="RaiseMoreClientSearch"
-                    searchClient={searchClient}
-                  >
-                    <SearchBox />
-                    <Hits />
-                  </InstantSearch> */}
-                                    <div className="mr-5 relative pt-3 w-full text-gray-400 focus-within:text-gray-600">
-                                        <DocSearch
-                                            appId={process.env.ALGOLIA_APPLICATION_ID}
-                                            indexName="RaiseMoreClientSearch"
-                                            apiKey={
-                                                process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY
-                                            }
-                                        />
-                                    </div>
-                                    {/* <div className="relative w-full text-gray-400 focus-within:text-gray-600">
-                    <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center">
-                      <MagnifyingGlassIcon
-                        className="h-5 w-5"
-                        aria-hidden="true"
-                      />
-                    </div>
-                    <input
-                      id="search-field"
-                      className="block h-full w-full border-transparent py-2 pl-8 pr-3 text-gray-900 placeholder-gray-500 focus:border-transparent focus:placeholder-gray-400 focus:outline-none focus:ring-0 sm:text-sm"
-                      placeholder="Search"
-                      type="search"
-                      name="search"
-                    />
-                  </div> */}
-                                </form>
-                            </div>
-
                             {isSignedIn ? (
                                 <>
+                                    <div className="flex flex-1">
+                                        <form
+                                            className="flex w-full md:ml-0"
+                                            action="#"
+                                            method="GET"
+                                        >
+                                            <div className="mr-5 relative pt-3 w-full text-gray-400 focus-within:text-gray-600">
+                                                <DocSearch
+                                                    appId={process.env.ALGOLIA_APPLICATION_ID}
+                                                    indexName="RaiseMoreClientSearch"
+                                                    apiKey={
+                                                        process.env
+                                                            .NEXT_PUBLIC_ALGOLIA_SEARCH_ONLY_API_KEY
+                                                    }
+                                                />
+                                            </div>
+                                        </form>
+                                    </div>
                                     <div className="ml-4 flex items-center md:ml-6 gap-5">
                                         <button
                                             type="button"
@@ -470,6 +449,7 @@ const Layout = ({ children }) => {
                                                     colorPrimary: "#2d28ff",
                                                 },
                                             }}
+                                            // signInUrl=
                                         />
                                     </div>
                                 </div>
