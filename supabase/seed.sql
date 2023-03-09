eace5e5cfa7a88cefb768dbf20119c636821f99d
jameswang <james.wang.dev712@gmail.com>
2023-03-09 12:55:33 -0500

    dunno why this was needed to get make running but there you go

--- supabase/seed.sql
+++ supabase/seed.sql
@@ -1,8 +1,28 @@
 -- Fix the issue with JWT user being type checked for UUID and corresponding ghost RLS policy
-ALTER TABLE storage.buckets DROP CONSTRAINT buckets_owner_fkey;
-ALTER TABLE storage.buckets ALTER COLUMN owner SET DATA TYPE text;
-ALTER TABLE storage.objects DROP CONSTRAINT objects_owner_fkey;
-ALTER TABLE storage.objects ALTER COLUMN owner SET DATA TYPE text;
-ALTER TABLE storage.objects DISABLE ROW LEVEL SECURITY;
-INSERT INTO storage.buckets (id,name,created_at,updated_at,public,avif_autodetection) VALUES ('imports','imports',now(),now(),TRUE,FALSE)
-ON CONFLICT (id) DO NOTHING;
+ALTER TABLE
+  storage.buckets DROP CONSTRAINT buckets_owner_fkey;
+
+ALTER TABLE
+  storage.buckets
+ALTER COLUMN
+  owner
+SET
+  DATA TYPE text;
+
+ALTER TABLE
+  storage.objects DROP CONSTRAINT objects_owner_fkey;
+
+ALTER TABLE
+  storage.objects
+ALTER COLUMN
+  owner
+SET
+  DATA TYPE text;
+
+ALTER TABLE
+  storage.objects DISABLE ROW LEVEL SECURITY;
+
+INSERT INTO
+  storage.buckets (id, name, created_at, updated_at, public)
+VALUES
+  ('imports', 'imports', now(), now(), TRUE) ON CONFLICT (id) DO NOTHING;
